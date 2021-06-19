334d9002d1a3ca98135598d9865789fd49595ff7
Niggel <niggelgame@gmail.com>
2021-06-19 13:44:47 +0200

    ci: fix android build

--- app/lib/utils/simple_bloc_observer.dart
+++ app/lib/utils/simple_bloc_observer.dart
@@ -6,6 +6,7 @@ class SimpleBlocObserver extends BlocObserver {
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
+    // print(transition);
     super.onTransition(bloc, transition);
   }
 
