5760290167cf00cdb0b820e68a2ff950d1eb9ce4
Niggel <niggelgame@gmail.com>
2021-06-19 10:49:46 +0200

    ci: fix app directory

--- app/lib/theming/theme.dart
+++ app/lib/theming/theme.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 class Co2Theme {
   static ThemeData lightTheme = ThemeData(
-      backgroundColor: Color(0xFFC1C9E8),
-      accentColor: Color(0xFFA2E889),
-      textTheme: TextTheme(
-          headline1: TextStyle(fontSize: 34.5, fontWeight: FontWeight.w700)));
+    backgroundColor: Color(0xFFC1C9E8),
+    accentColor: Color(0xFFA2E889),
+    textTheme: TextTheme(
+      headline1: TextStyle(fontSize: 34.5, fontWeight: FontWeight.w700),
+    ),
+  );
 }
