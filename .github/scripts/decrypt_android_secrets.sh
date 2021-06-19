3a906cd93a529704d94d69526efa6465c5260ce6
Niggel <niggelgame@gmail.com>
2021-06-19 13:31:49 +0200

    ci: rename decryption file

--- .github/scripts/decrypt_android_secrets.sh
+++ .github/scripts/decrypt_android_secrets.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-echo $(pwd)
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
--- app/lib/utils/simple_bloc_observer.dart
+++ app/lib/utils/simple_bloc_observer.dart
@@ -6,7 +6,6 @@ class SimpleBlocObserver extends BlocObserver {
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    // print(transition);
     super.onTransition(bloc, transition);
   }
 
