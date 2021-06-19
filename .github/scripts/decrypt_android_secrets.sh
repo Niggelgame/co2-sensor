b79bf76e79ca60465f770f0e781f558380b8e229
Niggel <niggelgame@gmail.com>
2021-06-19 13:07:15 +0200

    ci: add android build

---
+++ .github/scripts/decrypt_android_secrets.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
+--output app/android/android_keys.zip app/android/android_keys.zip.gpg && cd app/android && jar xvf android_keys.zip && cd -
