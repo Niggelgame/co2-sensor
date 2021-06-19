eb948fd745fbf96571aaf6f07b4a7ce684020099
Niggel <niggelgame@gmail.com>
2021-06-19 10:48:11 +0200

    ci: trigger iOS build CI

--- app/lib/theming/theme.dart
+++ app/lib/theming/theme.dart
@@ -1,16 +1,9 @@
-
-
 import 'package:flutter/material.dart';
 
 class Co2Theme {
   static ThemeData lightTheme = ThemeData(
-    backgroundColor: Color(0xFFC1C9E8),
-    accentColor: Color(0xFFA2E889),
-    textTheme: TextTheme(
-      headline1: TextStyle(
-        fontSize: 34.5,
-        fontWeight: FontWeight.w700
-      )
-    )
-  );
+      backgroundColor: Color(0xFFC1C9E8),
+      accentColor: Color(0xFFA2E889),
+      textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 34.5, fontWeight: FontWeight.w700)));
 }
